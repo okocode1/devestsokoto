0e070ec07c6ac63dee005fbd91ff1f13add3ff9c
Okocode <146639319+okocode1@users.noreply.github.com>
2023-11-11 10:11:21 +0100

    version 0.1 Devfestsokoto twitter challange
    This is the first version you can update it

---
+++ script.js
@@ -0,0 +1,212 @@
+const devfestTweets = [
+  "🚀 Today is the day! Excited for #devfestsokoto! Join us for a day of tech innovation and networking.",
+  "🎉 Don't miss out on the opportunity to learn and connect at #devfestsokoto. See you today!",
+  "👩‍💻 Today is the day, tech enthusiasts in Sokoto! #devfestsokoto is the place to be. Let's code and collaborate!",
+  "⏰ Ready to level up your skills? #devfestsokoto is the perfect place to do just that. Today's the day, RSVP now!",
+  "🌐 Today is the day! Get inspired, learn, and connect at #devfestsokoto. It's a tech extravaganza you won't want to miss!",
+  "🔓 Unlock the doors to endless possibilities today at #devfestsokoto. Join us for a day of tech exploration!",
+  "🤝 Connect with fellow developers, share ideas, and learn something new at #devfestsokoto. See you today!",
+  "🎤 Exciting speakers, engaging workshops, and a vibrant tech community – that's #devfestsokoto for you! Today's the day!",
+  "🚀 Tech enthusiasts unite! #devfestsokoto is the place to be for a day filled with knowledge and inspiration. See you today!",
+  "📅 Mark your calendars! #devfestsokoto is happening today, and it's going to be epic. Don't miss out!",
+  "🚀 Ready to dive into the world of tech? Join us at #devfestsokoto today and explore the possibilities!",
+  "👩‍💻 Calling all developers, designers, and tech enthusiasts! #devfestsokoto is the place to be today.",
+  "🎉 Exciting news: #devfestsokoto is happening today! Secure your spot for a day of tech brilliance.",
+  "👨‍💼 Join us today at #devfestsokoto for a tech-filled day that promises inspiration, learning, and collaboration!",
+  "🔍 Curious about the latest tech trends? #devfestsokoto is your chance to stay ahead of the curve. See you today!",
+  // ... repeat similar tweets with different content ...
+
+  // Tweets 16-30
+  "🚀 Elevate your coding skills today at #devfestsokoto. It's not just an event; it's an experience!",
+  "🌟 Are you passionate about tech? Join us at #devfestsokoto today to meet like-minded individuals and learn from experts.",
+  "💡 Exciting workshops, insightful talks, and endless networking opportunities – #devfestsokoto has it all! See you today!",
+  "💻 Tech enthusiasts, assemble! 🚀 #devfestsokoto is the ultimate gathering for those who dream in code. Today's the day!",
+  "🌐 Embark on a journey of learning and discovery at #devfestsokoto. Your tech adventure awaits today!",
+  "💻 Code, connect, and collaborate at #devfestsokoto. Don't miss the chance to be part of something extraordinary today!",
+  "🚀 Tech novices and experts alike, #devfestsokoto welcomes everyone. Let's celebrate the world of possibilities together today!",
+  "🚀 Discover the latest in tech trends and innovation at #devfestsokoto. It's where ideas come to life today!",
+  "🌐 Dive into the world of web development at #devfestsokoto today. Your gateway to a tech-driven future!",
+  "🤖 Excited about the future of AI? Join us at #devfestsokoto today for insights, discussions, and hands-on experiences!",
+  "👾 Techies, unite! 🤖 #devfestsokoto is the place to be for a day filled with knowledge, inspiration, and fun. See you today!",
+  "🌈 Join us today at #devfestsokoto to explore the intersections of technology and creativity. It's where innovation meets imagination!",
+  "🎉 Tech enthusiasts, mark your calendars! #devfestsokoto is your ticket to a day of learning and networking. Today's the day!",
+  "📊 Unlock the power of data today at #devfestsokoto. Join us for discussions, workshops, and hands-on activities!",
+  "🚀 Ready to hack the future? #devfestsokoto is your playground for creativity, coding, and collaboration. Today's the day!",
+
+  // ... repeat similar tweets with different content ...
+
+  // Tweets 31-45
+  "🤝 Connect with industry experts today at #devfestsokoto. It's the perfect opportunity to expand your professional network!",
+  "🎓 Tech novices and pros alike, #devfestsokoto has something for everyone. Don't miss this chance to level up your skills today!",
+  "📱 Unlock the secrets of successful app development today at #devfestsokoto. Join us for insights, tips, and hands-on experiences!",
+  "💡 From coding challenges to inspiring talks, #devfestsokoto is a day packed with opportunities for growth and learning. Today!",
+  "🚀 Join the tech revolution today at #devfestsokoto. It's not just an event; it's a movement towards a brighter, tech-driven future!",
+  "💻 Discover the latest trends in software development today at #devfestsokoto. It's where innovation takes center stage!",
+  "🔐 Are you passionate about cybersecurity? #devfestsokoto is the place to be for insights, discussions, and networking. Today!",
+  "🚀 Calling all aspiring entrepreneurs! #devfestsokoto is your chance to learn, connect, and turn your ideas into reality. Today!",
+  "👥 Tech enthusiasts of Sokoto, unite today at #devfestsokoto! Let's build a stronger, more connected tech community.",
+  "🌟 🚀 Dive into the world of emerging technologies today at #devfestsokoto. It's your gateway to the future!",
+  "💻 Join us today at #devfestsokoto for a day of coding challenges, hackathons, and collaborative problem-solving.",
+  "🚀 Ready to upgrade your tech skills today? #devfestsokoto is the perfect opportunity to learn from experts and connect with peers.",
+  "🎉 Exciting news for Sokoto's tech community! #devfestsokoto is back, and it's bigger and better than ever. See you today!",
+  "🤖 Tech enthusiasts, assemble! #devfestsokoto is your chance to be part of a community that celebrates innovation and creativity. Today!",
+  "📆 Mark your calendars for #devfestsokoto. It's not just an event; it's an experience that will shape your tech journey. Today!",
+
+  // ... repeat similar tweets with different content ...
+
+  // Tweets 46-60
+  "🌐 Eager to explore the world of IoT? #devfestsokoto is the perfect platform to dive into the exciting realm of connected devices. Today!",
+  "🚀 From AI to blockchain, #devfestsokoto covers it all. Join us for a day of tech talks, workshops, and hands-on experiences. Today!",
+  "📱 Calling all app developers! #devfestsokoto is your playground for learning, sharing ideas, and collaborating on innovative projects. Today!",
+  // ... tweets with updated content for today's emphasis ...
+
+  "🚀 Dive into the world of AR and VR today at #devfestsokoto. Join us for immersive experiences and cutting-edge tech talks!",
+  "🌟 Today is the day to connect with industry leaders at #devfestsokoto. Don't miss the chance to expand your professional network!",
+  "👥 Tech novices and pros alike, #devfestsokoto welcomes everyone. Today is your day to level up your skills and make lasting connections!",
+  "📱 Discover the secrets of successful app development today at #devfestsokoto. Join us for insights, tips, and hands-on experiences!",
+  "💻 From coding challenges to inspiring talks, #devfestsokoto is a day packed with opportunities for growth and learning. Today!",
+  "🌐 Join the tech revolution today at #devfestsokoto. It's not just an event; it's a movement towards a brighter, tech-driven future!",
+  "🚀 Ready to upgrade your tech skills today? #devfestsokoto is the perfect opportunity to learn from experts and connect with peers.",
+  "🎉 Exciting news for Sokoto's tech community! #devfestsokoto is back, and it's bigger and better than ever. See you today!",
+  "🤖 Tech enthusiasts, assemble! #devfestsokoto is your chance to be part of a community that celebrates innovation and creativity. Today!",
+  "📆 Mark your calendars for #devfestsokoto. It's not just an event; it's an experience that will shape your tech journey. Today!",
+  "🌐 Eager to explore the world of IoT? #devfestsokoto is the perfect platform to dive into the exciting realm of connected devices. Today!",
+  "🚀 From AI to blockchain, #devfestsokoto covers it all. Join us for a day of tech talks, workshops, and hands-on experiences. Today!",
+  "📱 Calling all app developers! #devfestsokoto is your playground for learning, sharing ideas, and collaborating on innovative projects. Today!",
+  "🚀 Don't miss the chance to experience live coding sessions at #devfestsokoto today. Witness the magic of tech unfold before your eyes!",
+  "🎤 Today is the day to hear from inspiring speakers at #devfestsokoto. Get ready for talks that will ignite your passion for tech!",
+
+  // Tweets 76-90
+  "🚀 Ready to explore the latest trends in software development? #devfestsokoto has you covered. Join us today for a tech-filled adventure!",
+  "🔒 Unlock the mysteries of cybersecurity today at #devfestsokoto. Dive into discussions, gain insights, and network with experts!",
+  "🚀 Calling all aspiring entrepreneurs! #devfestsokoto is your launchpad for success. Learn, connect, and turn your ideas into reality. Today!",
+  "👥 Tech enthusiasts of Sokoto, unite today at #devfestsokoto! Let's build a stronger, more connected tech community.",
+  "🌈 Join us today at #devfestsokoto to explore the intersections of technology and creativity. It's where innovation meets imagination!",
+  "🎉 Tech enthusiasts, mark your calendars! #devfestsokoto is your ticket to a day of learning and networking. Today's the day!",
+  "📊 Unlock the power of data today at #devfestsokoto. Join us for discussions, workshops, and hands-on activities!",
+  "🚀 Ready to hack the future? #devfestsokoto is your playground for creativity, coding, and collaboration. Today's the day!",
+  "🎉 Today is the day! Don't miss the chance to join us at #devfestsokoto. It's not just an event; it's a celebration of tech excellence!",
+  "📣 Tech enthusiasts, the wait is over! #devfestsokoto is happening today. Get ready for an unforgettable day of innovation and learning!",
+  "🚀 It's not just an event; it's a tech extravaganza! Join us today at #devfestsokoto for a day filled with inspiration and collaboration.",
+  "🌟 Today is your opportunity to learn, connect, and elevate your tech skills at #devfestsokoto. Don't miss out on this incredible experience!",
+  "💻 Ready to embark on a coding adventure? #devfestsokoto is your destination. Join us today for coding challenges and hackathons!",
+  "🔥 Today is the day to witness the fusion of tech and creativity at #devfestsokoto. Join us for a celebration of innovation!",
+  "🚀 Today's the day to explore the future of tech. Join us at #devfestsokoto for a glimpse into the latest advancements and breakthroughs!",
+
+  // Tweets 91-100
+  "🔥 Today is your chance to be part of tech history at #devfestsokoto. Join us for a day that will leave a lasting impact on your journey!",
+  "💡 Tech novices and pros, today is the day to come together at #devfestsokoto. Let's celebrate the diversity and brilliance of our community!",
+  "🎉 Ready to make memories and friends? Join us today at #devfestsokoto for a day of fun, learning, and forging meaningful connections!",
+  "🌐 Today is the day to dive into the ever-evolving world of technology at #devfestsokoto. Join us for insights, discussions, and exploration!",
+  "🚀 Today is the day we celebrate innovation, creativity, and the incredible minds of Sokoto's tech community. Join us at #devfestsokoto!",
+  "🌟 Don't miss the chance to connect with like-minded individuals at #devfestsokoto today. Your next collaboration or friendship could begin here!",
+  "💻 Today's the day to break barriers, learn new skills, and make a difference. Join us at #devfestsokoto for a tech-filled experience!",
+  "🎤 Today, we amplify the voices of tech enthusiasts at #devfestsokoto. Share your insights, learn from others, and make your mark on the community!",
+  "🌐 Today is the day we shape the future of tech in Sokoto. Join us at #devfestsokoto for a day that will leave a lasting impact!",
+  "🚀 Today, we invite you to be part of something extraordinary. Join us at #devfestsokoto for a day of inspiration, learning, and celebration!",
+
+  // Tweets 101-135
+  "🚀 Gear up for a day of coding brilliance at #devfestsokoto. Today's the day to unlock new possibilities and elevate your skills!",
+  "🌟 Dive into the world of tech with enthusiasm and curiosity at #devfestsokoto. Join us for a day filled with innovation and learning!",
+  "💡 Ready to embrace the future of technology? #devfestsokoto is your gateway to the latest trends and advancements. See you today!",
+  "💻 Calling all tech enthusiasts! #devfestsokoto is the hub of creativity, collaboration, and knowledge sharing. Don't miss out!",
+  "🌐 Explore the limitless possibilities of the digital world at #devfestsokoto today. It's more than an event; it's an experience!",
+  "🔥 Ignite your passion for tech at #devfestsokoto. Join us today for insightful talks, interactive workshops, and networking galore!",
+  "🚀 Today is your chance to connect with Sokoto's tech community. #devfestsokoto is where friendships and collaborations begin!",
+  "🎓 Tech novices, experts, and everyone in between – #devfestsokoto welcomes you. Elevate your skills and be part of something extraordinary!",
+  "📱 Ready to unravel the secrets of mobile app development? #devfestsokoto is your guide to success. Join us for a day of insights!",
+  "💻 Code, collaborate, and create at #devfestsokoto today. It's not just an event; it's a celebration of the tech community's brilliance!",
+  "🌟 Embark on a tech adventure at #devfestsokoto. From workshops to networking, it's the perfect platform for your journey in innovation!",
+  "🚀 Join us at #devfestsokoto for a day of groundbreaking discussions on AI, machine learning, and the future of technology!",
+  "🌐 Today is your day to shine in the tech world. Join us at #devfestsokoto and be part of the next wave of innovation!",
+  "🎉 Tech enthusiasts, mark your calendars! #devfestsokoto is the go-to event for those who dream of pushing the boundaries of possibility.",
+  "📊 Dive into the data-driven world at #devfestsokoto. Today's the day to explore analytics, insights, and the power of data!",
+  "🚀 Ready to hack challenges and unlock your potential? Join us at #devfestsokoto for coding challenges and hackathons!",
+  "👥 Connect, collaborate, and celebrate tech excellence at #devfestsokoto. Today is your day to be part of Sokoto's vibrant tech community!",
+  "🌈 Explore the intersection of creativity and technology at #devfestsokoto. It's where innovation meets imagination. See you today!",
+  "🎤 Get ready to be inspired by tech leaders at #devfestsokoto. Today's the day to hear stories that fuel your passion for innovation!",
+  "🚀 Elevate your coding skills and join the tech revolution at #devfestsokoto. It's not just an event; it's a movement!",
+  "🌟 Unleash your creativity in the world of emerging technologies at #devfestsokoto. Today is your day to explore the future!",
+  "💻 Calling all app developers! #devfestsokoto is your playground for learning, sharing ideas, and collaborating on groundbreaking projects. Today!",
+  "🚀 Today is your day to experience live coding sessions at #devfestsokoto. Witness the magic of tech unfold before your eyes!",
+  "🎉 Today is the day to hear from inspiring speakers at #devfestsokoto. Get ready for talks that will ignite your passion for tech!",
+  "🔥 Ready to explore the latest trends in software development? #devfestsokoto has you covered. Join us today for a tech-filled adventure!",
+  "🔒 Unlock the mysteries of cybersecurity today at #devfestsokoto. Dive into discussions, gain insights, and network with experts!",
+  "🚀 Calling all aspiring entrepreneurs! #devfestsokoto is your launchpad for success. Learn, connect, and turn your ideas into reality. Today!",
+  "👥 Tech enthusiasts of Sokoto, unite today at #devfestsokoto! Let's build a stronger, more connected tech community.",
+  "🌈 Join us today at #devfestsokoto to explore the intersections of technology and creativity. It's where innovation meets imagination!",
+  "🎉 Tech enthusiasts, mark your calendars! #devfestsokoto is your ticket to a day of learning and networking. Today's the day!",
+  "📊 Unlock the power of data today at #devfestsokoto. Join us for discussions, workshops, and hands-on activities!",
+  "🚀 Ready to hack the future? #devfestsokoto is your playground for creativity, coding, and collaboration. Today's the day!",
+  "🎉 Today is the day! Don't miss the chance to join us at #devfestsokoto. It's not just an event; it's a celebration of tech excellence!",
+  "📣 Tech enthusiasts, the wait is over! #devfestsokoto is happening today. Get ready for an unforgettable day of innovation and learning!",
+  "🚀 It's not just an event; it's a tech extravaganza! Join us today at #devfestsokoto for a day filled with inspiration and collaboration!",
+  "🌟 Today is your opportunity to learn, connect, and elevate your tech skills at #devfestsokoto. Don't miss out on this incredible experience!",
+];
+
+const uniqueDevfestTweets = [...new Set(devfestTweets)];
+
+// Access a random tweet
+const xEl = document.querySelector(".twitter");
+
+const tweets = () => {
+  uniqueDevfestTweets.map((tweet) => {
+    const tweetText = encodeURIComponent(tweet + "\n" + "@gdgsokoto");
+
+    const tweetLink = document.createElement("a");
+    tweetLink.classList.add("twitter-share-button");
+    tweetLink.href = `https://twitter.com/intent/tweet?text=${tweetText}`;
+    tweetLink.setAttribute("data-size", "large");
+    tweetLink.setAttribute("target", "_blank");
+    tweetLink.textContent = "Tweet";
+
+    // Add a click event listener to add the 'visited' class when clicked
+    tweetLink.addEventListener("click", () => {
+      tweetLink.classList.add("visited");
+    });
+
+    const tweetParagraph = document.createElement("p");
+    tweetParagraph.appendChild(document.createTextNode(tweet));
+    tweetParagraph.appendChild(tweetLink);
+
+    xEl.appendChild(tweetParagraph);
+  });
+};
+
+tweets();
+
+///////////////////////////////////////
+// Change the body background color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function isDark(color) {
+  const rgb = parseInt(color.substring(1), 16);
+  const r = (rgb >> 16) & 0xff;
+  const g = (rgb >> 8) & 0xff;
+  const b = (rgb >> 0) & 0xff;
+
+  // Using the HSP color model to determine brightness
+  const brightness = Math.sqrt(
+    0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b)
+  );
+
+  // Return true if the background is considered dark
+  return brightness < 128;
+}
+
+function updateColors() {
+  const bgColor = getRandomColor();
+  document.body.style.backgroundColor = bgColor;
+
+  // Set text color based on background brightness
+  document.body.style.color = isDark(bgColor) ? "#fff" : "#333";
+}
+
+setInterval(updateColors, 2000);
